332a38addf1bc987ee1195aaaa7cb32c5a7114ae
huylam0703 <huylamnhat244@gmail.com>
2025-05-31 01:08:04 +0700

    first commit

---
+++ src/test/java/com/learn/indentityservice/IndentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.indentityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndentityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
